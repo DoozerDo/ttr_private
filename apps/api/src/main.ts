e9644928dc62960e524acd60c6cea8b7114886b3
dcanny <49954530+dcanny@users.noreply.github.com>
2025-11-29 14:11:49 -0800

    Add support for dev and prod ENV's

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
+  const corsOrigin =
+    config.get<string>('CORS_ORIGIN') ?? 'http://localhost:3000';
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: corsOrigin,
   });
 
-  await app.listen(process.env.PORT ?? 3001);
+  const port = config.get<number>('PORT') ?? 3001;
+
+  await app.listen(port);
 }
 bootstrap();
